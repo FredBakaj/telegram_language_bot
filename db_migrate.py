f081f92ba95e435d539888fc4e18c8ca6c313365
Фёдор Бакай <92712983+FredBakaj@users.noreply.github.com>
2023-04-02 09:17:54 +0000

    fix migrate database

--- db_migrate.py
+++ db_migrate.py
@@ -1,13 +1,37 @@
+import argparse
+import os
+from peewee import SqliteDatabase, PostgresqlDatabase
 from peewee_migrate import Router
-from peewee import SqliteDatabase
 
-router = Router(SqliteDatabase('sqlite:///data/database.sqlite3'))
 
-# Create migration
-router.create('migration_name')
+def main():
+  parser = argparse.ArgumentParser(
+    description='Database migration tool for Peewee ORM.')
+  parser.add_argument('--database', required=True, help='Database URL.')
+  parser.add_argument('--directory',
+                      required=True,
+                      help='Migration directory.')
+  args = parser.parse_args()
 
-# Run migration/migrations
-router.run('migration_name')
+  if args.database.startswith('sqlite://'):
+    # SQLite database URL
+    db = SqliteDatabase(
+      "/home/runner/telegramlanguagebot/data/database.sqlite3")
+  else:
+    # Postgres database URL
+    db = PostgresqlDatabase.from_url(args.database)
 
-# Run all unapplied migrations
-# router.run()
+  router = Router(db)
+  # router.create('migration_name')
+
+  # Run migration/migrations
+  router.run('migration_name')
+
+  print('Migrations applied successfully.')
+
+
+if __name__ == '__main__':
+  main()
+  # from pathlib import Path
+  # DIR = Path(__file__).absolute().parent
+  # print(DIR)
