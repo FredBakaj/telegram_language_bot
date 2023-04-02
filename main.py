670e321c72b2c3816e87fa67e2fcd4beada71247
Fred <bakajfedawork@gmail.com>
2023-04-02 18:52:52 +0300

    add webserver

--- main.py
+++ main.py
@@ -4,31 +4,34 @@ from aiogram import executor
 from bot.commands import set_default_commands
 from loader import dp, bot, config
 from utils.misc.logging import logger
+from webserver import keep_alive
 
 
 async def on_startup(dispatcher: Dispatcher):
-  logger.info('Bot startup')
+    logger.info('Bot startup')
 
-  for admin_id in config.ADMINS:
-    await bot.send_message(admin_id, 'bot started')
+    for admin_id in config.ADMINS:
+        await bot.send_message(admin_id, 'bot started')
 
-  await set_default_commands()
+    await set_default_commands()
 
 
 async def on_shutdown(dispatcher: Dispatcher):
-  logger.warning('Shutting down..')
+    logger.warning('Shutting down..')
 
-  await dp.storage.close()
-  await dp.storage.wait_closed()
+    await dp.storage.close()
+    await dp.storage.wait_closed()
 
-  logger.warning('Bye!')
+    logger.warning('Bye!')
 
 
 if __name__ == '__main__':
-  from bot.middlewares import setup_middleware
-  from bot import filters, handlers
-  from bot import states
-  __all__ = ["dp"]
-  setup_middleware(dp)
+    from bot.middlewares import setup_middleware
+    from bot import filters, handlers
+    from bot import states
 
-  executor.start_polling(dp, on_startup=on_startup)
+    __all__ = ["dp"]
+    setup_middleware(dp)
+
+    keep_alive()
+    executor.start_polling(dp, on_startup=on_startup)
