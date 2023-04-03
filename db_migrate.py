b5558eb99daa9ff123fc84c6e1564de3a0a33ba6
Фёдор Бакай <>
2023-04-03 17:11:13 +0000

    add server file

--- db_migrate.py
+++ db_migrate.py
@@ -1,37 +1,11 @@
-import argparse
-import os
-from peewee import SqliteDatabase, PostgresqlDatabase
 from peewee_migrate import Router
+from peewee import SqliteDatabase
 
+db = "/home/runner/telegramlanguagebot/data/database.sqlite3"
+router = Router(SqliteDatabase(db))
 
-def main():
-  parser = argparse.ArgumentParser(
-    description='Database migration tool for Peewee ORM.')
-  parser.add_argument('--database', required=True, help='Database URL.')
-  parser.add_argument('--directory',
-                      required=True,
-                      help='Migration directory.')
-  args = parser.parse_args()
+# # Create migration
+# router.create('migration_name')
 
-  if args.database.startswith('sqlite://'):
-    # SQLite database URL
-    db = SqliteDatabase(
-      "/home/runner/telegramlanguagebot/data/database.sqlite3")
-  else:
-    # Postgres database URL
-    db = PostgresqlDatabase.from_url(args.database)
-
-  router = Router(db)
-  # router.create('migration_name')
-
-  # Run migration/migrations
-  router.run('migration_name')
-
-  print('Migrations applied successfully.')
-
-
-if __name__ == '__main__':
-  main()
-  # from pathlib import Path
-  # DIR = Path(__file__).absolute().parent
-  # print(DIR)
+# Run migration/migrations
+router.run('002_Init.py')
