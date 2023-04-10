3a6239f95e1e09717fc8f3d94a4a61ad0762fdf6
Fred <bakajfedawork@gmail.com>
2023-04-10 11:56:35 +0300

    fix bug with delete sentence and change sentence in localization

--- bot/keyboards/inline/sentence/delete_sentence.py
+++ bot/keyboards/inline/sentence/delete_sentence.py
@@ -6,6 +6,6 @@ def get_delete_sentence_inline_markup():  # TODO refactory
     markup = InlineKeyboardMarkup()
 
     markup.add(InlineKeyboardButton(_('Yes'), callback_data='delete_select_sentence_yes'),
-               InlineKeyboardButton(_('No'), callback_data='delete_select_sentence_yes'))
+               InlineKeyboardButton(_('No'), callback_data='delete_select_sentence_no'))
 
     return markup
